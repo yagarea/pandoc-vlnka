6b466f1476cb86930eace78fb58562cb584df40b
Jan Černý <jc@ucw.cz>
2023-09-26 21:02:01 +0200

    implement pandoc AST support

--- vlnka.hs
+++ vlnka.hs
@@ -1,5 +1,8 @@
 import Data.Char
 import Data.String
+import Text.Pandoc.JSON
+import qualified Data.Text as T
+import Numeric
 
 ---------------------------------------------------------------------------------------
 -- Jednoslabíčné předložky
@@ -43,8 +46,13 @@ isJednoslabicnaPredlozka potentialPredlozka = case potentialPredlozka of
 -- čísla s jednotkou
 
 isThisStringNumber :: String -> Bool
-isThisStringNumber testedString = (all isNumber testedString) && ((length testedString) /= 0)
+isThisStringNumber testedString =
+    case (readFloat testedString) of
+        [(num, "")] -> True
+        _ -> False
 
+endsWithNumber :: String -> Bool
+endsWithNumber testedString = isThisStringNumber (last (words testedString))
 
 isThisStringUnit :: String -> Bool
 isThisStringUnit testedString = case testedString of
@@ -59,7 +67,7 @@ isThisStringUnit testedString = case testedString of
     _ -> False
 
 isNumberWithUnit :: (String, String) -> Bool
-isNumberWithUnit (potentialNumber, potentialMark) = and [(isThisStringNumber potentialNumber), (isThisStringUnit potentialMark)]
+isNumberWithUnit (potentialNumber, potentialMark) = and [(endsWithNumber potentialNumber), (isThisStringUnit potentialMark)]
 
 ---------------------------------------------------------------------------------------
 -- značka s číslem
@@ -108,8 +116,43 @@ shouldHaveVlnka pairOfWords = foldr (||) False [
     isMarkWithNumber pairOfWords]
 
 
-vlnka :: String -> String
-vlnka text = strip (foldr (++) "" (map addGlueToWord (getPairsOfListMemebers(words text))))
+vlnka_ast :: [Inline] -> [Inline]
+vlnka_ast [] = []
+vlnka_ast [a] = [a]
+vlnka_ast [a,b] = [a,b]
+vlnka_ast (Str a: Text.Pandoc.JSON.Space : Str b : rest) |  shouldHaveVlnka (T.unpack a, T.unpack b) =  vlnka_ast ((Str (T.concat [a, T.pack "\160", b])) : rest )
+vlnka_ast (a : rest) = a : vlnka_ast rest
 
-main = interact vlnka
+vlnka :: T.Text -> T.Text
+vlnka text = T.pack (vlnka_str (T.unpack text))
+
+vlnka_str :: String -> String
+vlnka_str text = strip (foldr (++) "" (map addGlueToWord (getPairsOfListMemebers(words text))))
+
+--vlnka_inline :: Inline -> Inline
+--vlnka_inline (Str text) = Str (vlnka text)
+--vlnka_inline (Emph inlines) = Emph (map vlnka_inline inlines)
+--vlnka_inline (Strong inlines) = Strong (map vlnka_inline inlines)
+--vlnka_inline (Strikeout inlines) = Strikeout (map vlnka_inline inlines)
+--vlnka_inline (Superscript inlines) = Superscript (map vlnka_inline inlines)
+--vlnka_inline (Subscript inlines) = Subscript (map vlnka_inline inlines)
+--vlnka_inline (SmallCaps inlines) = SmallCaps (map vlnka_inline inlines)
+--vlnka_inline (Quoted quoteType inlines) = Quoted quoteType (map vlnka_inline inlines)
+--vlnka_inline (Cite citations inlines) = Cite citations (map vlnka_inline inlines)
+--vlnka_inline (Code attr text) = Code attr text
+--vlnka_inline (Text.Pandoc.JSON.Space) = Text.Pandoc.JSON.Space
+--vlnka_inline (SoftBreak) = SoftBreak
+--vlnka_inline (LineBreak) = LineBreak
+--vlnka_inline (Math mathType text) = Math mathType text
+--vlnka_inline (RawInline format text) = RawInline format text
+--vlnka_inline (Link attr inlines target) = Link attr (map vlnka_inline inlines) target
+--vlnka_inline (Image attr inlines target) = Image attr (map vlnka_inline inlines) target
+--vlnka_inline (Note blocks) = Note blocks
+--vlnka_inline (Span attr inlines) = Span attr (map vlnka_inline inlines)
+
+
+
+
+
+main = toJSONFilter vlnka_ast
 
